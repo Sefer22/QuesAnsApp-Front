06f127a0b16646954d34251ad5d6b71999326716
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-20 01:20:01 +0400

    developed

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FormControl } from '@mui/material'
 import { Input } from '@mui/material'
-import { InputLabel } from '@mui/material'
+import { InputLabel, Button } from '@mui/material'
 
 function Auth() {
     return (
@@ -10,6 +10,13 @@ function Auth() {
             <Input />
             <InputLabel style={{ top: 80 }}>Password</InputLabel>
             <Input style={{ top: 40 }} />
+            <Button variant="contained"
+                style={{
+                    marginTop: 60,
+                    background: 'linear-gradient(45deg,#2196F3 30%,#21cbf3 90%)',
+                    color: 'white'
+                }}
+            >Register</Button>
         </FormControl>
     )
 }
