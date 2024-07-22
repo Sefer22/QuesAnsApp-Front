3ea269f3892c2f328e4fec54c12f41fca0db34f7
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-22 14:28:58 +0400

    developed

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,34 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 function Avatar() {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                sx={{ height: 140 }}
-                alt="User Avatar"
-                image="/avatars/avatar0.png"
-                title="User Avatar"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Username
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    User Info
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Change Avatar</Button>
-            </CardActions>
-        </Card>
+        <div>
+            <Card sx={{ maxWidth: 345, margin: 50 }}>
+                <CardMedia
+                    sx={{ height: 280 }}
+                    alt="User Avatar"
+                    image="/avatars/avatar0.png"
+                    title="User Avatar"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Username
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        User Info
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={handleOpen}>Change Avatar</Button>
+                </CardActions>
+            </Card>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                }}>
+                    <Typography id="parent-modal-title" variant="h6" component="h2">
+                        Hello
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
     );
 }
 
-export default Avatar
+export default Avatar;
