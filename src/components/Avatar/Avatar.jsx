082f51b218ae732fd7f4450cfa742e132a1ef94d
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-22 14:04:25 +0400

    developed

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -13,21 +12,20 @@ function Avatar() {
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
                 sx={{ height: 140 }}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="green iguana"
+                alt="User Avatar"
+                image="/avatars/avatar0.png"
+                title="User Avatar"
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+                    Username
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    User Info
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Button size="small">Change Avatar</Button>
             </CardActions>
         </Card>
     );
