19144db6541c08f1c0d589107c0bc745d30a05d8
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-27 13:19:06 +0400

    developed

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -5,27 +5,56 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 function Avatar() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                sx={{ height: 140 }}
-                image="/avatars/avatar0.png"
-                title="User Avatar"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Username
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    User Info
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Change Avatar</Button>
-            </CardActions>
-        </Card>
+        <div>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 300 }}
+                    image="/avatars/avatar0.png"
+                    title="User Avatar"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Username
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        User Info
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Change Avatar</Button>
+                </CardActions>
+            </Card>
+            <Button onClick={handleOpen}>Open modal</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Hello
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Welcome!!
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
     )
 }
 
