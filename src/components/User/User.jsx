6e31047239e1146dc39c25be8aa30ec6054d139d
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-08-01 14:32:09 +0400

    developed

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -7,6 +7,31 @@ import UserActivity from '../UserActivity/UserActivity';
 function User() {
 
     const { userId } = useParams();
+    const [user, setUser] = useState();
+
+    const savePost = () => {
+        fetch("http://localhost:8080/users/" + userId,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": localStorage.getItem("tokenKey")
+                },
+            })
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setUser(result);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
+    }
+
+
+
 
     return (
         <div style={{ display: 'flex' }}>
