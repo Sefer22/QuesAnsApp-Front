819f440803d781a4b48a7d00069dce26c4e27e8d
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-08-03 02:05:06 +0400

    developed

--- src/components/services/HttpService.jsx
+++ src/components/services/HttpService.jsx
@@ -11,3 +11,30 @@ export const PostWithAuth = (url, body) => {
 
     return request
 }
+
+export const PutWithAuth = (url, body) => {
+
+    var request = fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("tokenKey"),
+        },
+        body: JSON.stringify(body),
+    })
+
+    return request
+}
+
+export const GetWithAuth = (url) => {
+
+    var request = fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("tokenKey"),
+        },
+    })
+
+    return request
+}
